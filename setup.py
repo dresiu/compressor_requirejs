a05becbc7d11b294199e93741216fee572839c7f
Andrzej Przybyszewski <andrzejp@man.poznan.pl>
2014-04-15 13:25:58 +0200

    fixed setup file

--- setup.py
+++ setup.py
@@ -12,15 +12,16 @@ setup(
     package_data = {
         "compressor_requirejs": [
             "resources/*.js"
-        ],
+        ]
     },
-    version='1.1',
+    version='1.1.1',
+    license='LICENSE.txt',
     description='Library for django compressor, which helps to build requirejs',
     long_description=read('README.rst'),
     author='Andrzej Przybyszewski',
     author_email='mcendrju@gmail.com',
     url='https://github.com/dresiu/compressor_requirejs',
-    download_url='https://github.com/dresiu/compressor_requirejs/tarball/1.1',
+    download_url='https://github.com/dresiu/compressor_requirejs/tarball/1.1.1',
     keywords=['compressor_requirejs', 'django_compressor', 'django', 'compressor', 'requirejs'],
     requires=['django_compressor', 'PyExecJs'],
     classifiers=["Environment :: Web Environment",
